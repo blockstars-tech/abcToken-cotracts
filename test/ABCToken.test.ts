c61fde482063df23b1f0e468fbdaf935d3cf9ba3
Alexandr Martirosyan <alexandr.martirosyan2000@gmail.com>
2021-12-06 16:51:44 +0400

    end version of contracts

--- test/ABCToken.test.ts
+++ test/ABCToken.test.ts
@@ -3,50 +3,34 @@ import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import BN from "bn.js";
 
-const truffleAssert = require("truffle-assertions");
-
 import {
   ABCTokenContract,
   ABCTokenInstance,
   IUniswapV2Router02Contract,
   IUniswapV2Router02Instance,
 } from "../typechain-types";
-import { WBNBContract, WBNBInstance } from "../typechain-types/WBNB";
 
 chai.use(chaiAsPromised).should();
 const { expect, assert } = chai;
 
 const ABCToken: ABCTokenContract = artifacts.require("TestABC");
 const IUniswapV2Router: IUniswapV2Router02Contract = artifacts.require("IUniswapV2Router02");
-const WBNB: WBNBContract = artifacts.require("WBNB");
 
 const ten = new BN("10");
 const tenPow8 = ten.pow(new BN("8"));
-const tenPow18 = ten.pow(new BN("18"));
 
 const devTokenFeePercent = new BN("36");
 const devBNBFeePercent = new BN("12");
 const buyBackFeePercent = new BN("28");
 const liquidityFeePercent = new BN("24");
 
-const devAddress = "0xa670a43859bba57da9f0a275b601a3f0acccd41a";
-
-const coreTeamAddress = "0xd6bd0aa9ec3b00a11c9b56263ba730d3c1a82b18";
-const advisorsAddress = "0x6148e01353ef1104ba85dde9b60675a9d61b61a1";
-const reserveAddress = "0x442c53578def2ba3e0e3d402907ba2e6ce204499";
-const stakingAddress = "0x3aa9c623b4f6692f1b1c710899094548cc8fb316";
-const ecosystemAddress = "0x1022d7d2c37281af0af8068639211e0f6b09271f";
-const playToEarnAddress = "0xbf4f5ca51f777995f60e1f6a7e488787dc82c524";
-
 contract("ABCToken", (accounts) => {
-  const [deployer, user1, user2] = accounts;
+  const [deployer] = accounts;
 
   let router: IUniswapV2Router02Instance;
   let token: ABCTokenInstance;
-  let wbnb: WBNBInstance;
   before("Init router and WBNB", async () => {
     router = await IUniswapV2Router.at("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
-    wbnb = await WBNB.at("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2");
   });
 
   beforeEach("Deploy ABC Token", async () => {
